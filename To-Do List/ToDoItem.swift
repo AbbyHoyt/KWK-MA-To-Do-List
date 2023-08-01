c4170aa9ea441884246d69b08537db042d103b7a
Abby Hoyt <abby.hoyt@comcast.net>
2023-08-01 11:43:45 -0700

    Completed Iteration 0.

---
+++ To-Do List/ToDoItem.swift
@@ -0,0 +1,18 @@
+//
+//  ToDoItem.swift
+//  To-Do List
+//
+//  Created by Abby Hoyt on 8/1/23.
+//
+
+import Foundation
+
+class ToDoItem {
+    var title = ""
+    var isImportant = false
+    
+    init(title: String, isImportant: Bool = false) {
+        self.title = title
+        self.isImportant = isImportant
+    }
+}
