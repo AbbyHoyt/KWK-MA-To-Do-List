9155c51e1bc6f5a52143197588db7e7b995ff77c
Abby Hoyt <abby.hoyt@comcast.net>
2023-08-02 11:31:05 -0700

    Completed Iteration 2.

--- To-Do List/ContentView.swift
+++ To-Do List/ContentView.swift
@@ -9,7 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State var toDoItems: [ToDoItem] = []
+    @Environment(\.managedObjectContext) var context
+    
+    // @State var toDoItems: [ToDoItem] = []
+   @FetchRequest(entity: ToDo.entity(), sortDescriptors: [ NSSortDescriptor(keyPath: \ToDo.id, ascending: false) ])
+    var toDoItems: FetchedResults<ToDo>
+                   
     @State private var showNewTask = false
     
     var body: some View {
@@ -20,13 +25,6 @@ struct ContentView: View {
                     .fontWeight(.black)
                 
                 Spacer()
-                
-               /* Button(action: {
-                    self.showNewTask = true
-                }) {
-                Text("+")
-                }
-                .font(.system(size: 40)) */
                 
                 Button(action: {
                     self.showNewTask = true
@@ -45,17 +43,31 @@ struct ContentView: View {
             ForEach(toDoItems) { toDoItem in
                 // Text(toDoItem.title)
                 if toDoItem.isImportant == true {
-                    Text("‼️ " + toDoItem.title)
+                    Text("‼️ " + (toDoItem.title ?? "No title"))
                 }
                 else {
-                    Text(toDoItem.title)
+                    Text(toDoItem.title ?? "No title")
                 }
             }
+            .onDelete(perform: deleteTask)
         }
         .listStyle(.plain)
         
         if showNewTask {
-            NewToDoView(title: "", isImportant: false, toDoItems: $toDoItems, showNewTask: $showNewTask)
+            // NewToDoView(title: "", isImportant: false, toDoItems: $toDoItems, showNewTask: $showNewTask)
+            NewToDoView(title: "", isImportant: false, showNewTask: $showNewTask)
+        }
+    }
+    private func deleteTask(offsets: IndexSet) {
+        withAnimation {
+            offsets.map { toDoItems[$0] }.forEach(context.delete)
+            
+            do {
+                try context.save()
+            }
+            catch {
+                print(error)
+            }
         }
     }
 }
